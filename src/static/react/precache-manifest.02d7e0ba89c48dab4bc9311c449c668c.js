3c05977b27e020fef0e098af46ee00fbc610035d
harryyy27 <harryyy27@gmail.com>
2019-05-11 17:36:15 +0100

    flask react server set up

---
+++ src/static/react/precache-manifest.02d7e0ba89c48dab4bc9311c449c668c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad07f478d2be79b03ca6cc0cf8f2fef0",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "c02106475103159a6e5b",
+    "url": "/static/react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c02106475103159a6e5b",
+    "url": "/static/react/static/js/main.51890010.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/static/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
